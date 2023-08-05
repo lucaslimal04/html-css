110b67d931e5a9e6bcf4690504b465bab1f04737
Lucas Lima Rodrigues <138540444+lucaslimal04@users.noreply.github.com>
2023-08-05 12:06:01 -0300

    js

---
+++ exercicios/ex026/mq005/main.js
@@ -0,0 +1,27 @@
+
+var menuButton = document.querySelector('#burguer');
+var menuItens = document.querySelector('#itens');
+
+menuButton.addEventListener('click', clickMenu);
+
+window.addEventListener('resize', mudouTamanho);
+
+function clickMenu() {
+    if (menuItens.style.display == 'block') {
+        menuItens.style.display = 'none';
+    }
+    else {
+        menuItens.style.display ='block';
+    }
+
+}
+
+function mudouTamanho() {
+    if (window.innerWidth >= 768) {
+        menuItens.style.display = 'block';
+    }
+    else {
+        menuItens.style.display = 'none';
+    }
+}
+mudouTamanho();
